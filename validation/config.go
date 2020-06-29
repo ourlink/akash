e5310c815e8af57111288066f66ab737332b7d78
Doug F <sales@ourlink.net>
2020-06-29 15:02:06 -0500

    Lower minimum prices to 0

--- validation/config.go
+++ validation/config.go
@@ -16,7 +16,7 @@ type ValConfig struct {
 	MinUnitMemory  uint `env:"AKASH_MIN_UNIT_MEMORY"   envDefault:"1024"` // 1Mi
 	MinUnitStorage uint `env:"AKASH_MIN_UNIT_STORAGE"  envDefault:"1024"` // 1Mi
 	MinUnitCount   uint `env:"AKASH_MIN_UNIT_COUNT"    envDefault:"1"`
-	MinUnitPrice   uint `env:"AKASH_MIN_UNIT_PRICE"    envDefault:"1"`
+	MinUnitPrice   uint `env:"AKASH_MIN_UNIT_PRICE"    envDefault:"0"`
 
 	MaxGroupCount int `env:"AKASH_MAX_GROUP_COUNT" envDefault:"10"`
 	MaxGroupUnits int `env:"AKASH_MAX_GROUP_UNITS" envDefault:"10"`
@@ -25,7 +25,7 @@ type ValConfig struct {
 	MaxGroupMemory  int64 `env:"AKASH_MAX_GROUP_MEMORY"    envDefault:"1073741824"` // 1Gi
 	MaxGroupStorage int64 `env:"AKASH_MAX_GROUP_STORAGE"   envDefault:"5368709120"` // 5Gi
 
-	MinGroupMemPrice int64 `env:"AKASH_MEM_PRICE_MIN" envDefault:"1"`
+	MinGroupMemPrice int64 `env:"AKASH_MEM_PRICE_MIN" envDefault:"0"`
 	MaxGroupMemPrice int64 `env:"AKASH_MEM_PRICE_MAX" envDefault:"150"`
 }
 
